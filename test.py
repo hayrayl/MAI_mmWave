2392d6443fa425b6154434879db917b4307f0943
REU <hcrayl@purdue.edu>
2025-07-14 15:55:08 -0400

    setting up the basic structureÂ

---
+++ test.py
@@ -0,0 +1,5 @@
+import torch
+print("Is CUDA available:", torch.cuda.is_available())
+if torch.cuda.is_available():
+    print("Number of GPUs:", torch.cuda.device_count())
+    print("Current GPU name:", torch.cuda.get_device_name(0))
