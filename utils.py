2fd4e5359475e736c90a36b2864f4a75680f5c8e
hayrayl <hcrayl@purdue.edu>
2025-08-07 14:39:46 -0400

    Delete utils.py

--- utils.py
+++
@@ -1,8 +0,0 @@
-### simple code to store any miscellaneous functions we might have
-##
-import json
-
-def read_json(file):
-    with open(file) as json_file:
-        data = json.load(json_file)
-    return data
